eb75c6c59fd2958a16d20fcdb0684c62e25660dc
Asmae <154051652+asmae-hanine@users.noreply.github.com>
2024-01-14 20:25:26 +0100

    changed the nav color

--- imports/footer.php
+++ imports/footer.php
@@ -28,8 +28,8 @@
 
     <!-- Copyright -->
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-        © 2020 Copyright:
-        <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+        &copy; 2024
+        <a class="text-white" href="https://mdbootstrap.com/">AsmaeHanine</a>
     </div>
     <!-- Copyright -->
 </footer>
--- imports/header.php
+++ imports/header.php
@@ -2,7 +2,7 @@
 <div>
     <div class="header-blue">
         <nav class="navbar navbar-dark navbar-expand-md navigation-clean-search">
-            <div class="container"><a class="navbar-brand" href="#">Otako's Shop</a>
+            <div class="container-fluid"><a class="navbar-brand" href="#">Otako's Shop</a>
                 <!-- <button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button> -->
                 <div class="collapse navbar-collapse" id="navcol-1">
                     <ul class="nav navbar-nav">
@@ -14,7 +14,7 @@
 
                     <form class="form-inline mr-auto" target="_self">
                         <!-- search bar -->
-                        <div class="form-group"><label for="search-field"><i class="fa fa-search"></i></label><input class="form-control search-field" type="search" name="search" id="search-field"></div>
+                        <div class="form-group"><label for="search-field"></label></div>
                         <!-- login and sign up buttons  -->
                     </form>
                     <span class="navbar-text"> <a href="./login-page.php" class="login">Log In</a></span>
